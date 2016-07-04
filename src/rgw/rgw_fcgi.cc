786909d68c1cd7fc5b77a3e794de9266ea55d5aa
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2016-07-04 16:02:12 +0200

    rgw: unify handling Content-Length in FactCGI and civetweb frontends.
    Fixes: http://tracker.ceph.com/issues/16602
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- src/rgw/rgw_fcgi.cc
+++ src/rgw/rgw_fcgi.cc
@@ -44,10 +44,17 @@ int RGWFCGX::send_content_length(uint64_t len)
 {
   /*
    * Status 204 should not include a content-length header
-   * RFC7230 says so
+   * RFC7230 says so.
+   *
+   * Same goes for status 304: Not Modified
+   *
+   * 'If a cache uses a received 304 response to update a cache entry,'
+   * 'the cache MUST update the entry to reflect any new field values'
+   * 'given in the response.'
    */
-  if (status_num == 204)
+  if (status_num == 204 || status_num == 304) {
     return 0;
+  }
 
   char buf[21];
   snprintf(buf, sizeof(buf), "%" PRIu64, len);
